6ed4732383436904fe83c4f73e516b18b2a3063b
erkn <erik@vonbrun.org>
2025-04-07 10:20:17 +0200

    try to get api-doc to run automat

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,17 +1,21 @@
 # Configuration file for the Sphinx documentation builder.
 #
 # -- Project information -----------------------------------------------------
+import os, sys
 
 project = "CAD_to_OpenMC Project"
 copyright = "2025, CAD_to_OpenMC-team"
 author = "ebknudsen the Docs core team"
 
+sys.path.insert(0, os.path.abspath('../src'))
+
 master_doc = 'index'
 
 extensions = [
     "sphinx.ext.duration",
-    "sphinx.ext.napoloeon",
+    "sphinx.ext.napoleon",
     "sphinx.ext.doctest",
+    "sphinx.ext.apidoc",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
